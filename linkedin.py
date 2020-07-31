d5fe57372a24b6d93b2eeb206ced71d43ca86a9e
Maghil <mmaghil2k@gmail.com>
2020-07-31 21:29:36 +0530

    added yml deserializer

--- linkedin.py
+++ linkedin.py
@@ -1,9 +1,13 @@
 import time
+import yaml
 import selenium
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
+
+with open(r'linkedIn.yml') as file:
+    user = yaml.full_load(file)
 
 try:
     driver = webdriver.Chrome(ChromeDriverManager().install())
@@ -11,8 +15,8 @@ try:
     #login page
     driver.get("https://www.linkedin.com/login?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin")
 
-    driver.find_element_by_id("username").send_keys(#usrname)
-    driver.find_element_by_id ("password").send_keys(#pwd)
+    driver.find_element_by_id("username").send_keys(user["email"])
+    driver.find_element_by_id ("password").send_keys(user["pwd"])
     driver.find_element_by_tag_name("button").click()
 
     #API to add new certificate
