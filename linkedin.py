112c3db8230ee2e7398ad961a42f7f971926fde0
Maghil <mmaghil2k@gmail.com>
2020-07-31 16:46:12 +0530

    edited config.yml 2.added requirements.txt 3.edited linkedin.py ->using API now , added webdriver_manager

--- linkedin.py
+++ linkedin.py
@@ -3,47 +3,31 @@ import selenium
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
 
 try:
-    driver = webdriver.Chrome("C:\Program Files\chromedriver_win32\chromedriver.exe")
-
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    
+    #login page
     driver.get("https://www.linkedin.com/login?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin")
 
-    driver.find_element_by_id("username").send_keys(#usrname)
-    driver.find_element_by_id ("password").send_keys(#password)
+    driver.find_element_by_id("username").send_keys("mmaghil2k@gmail.com")
+    driver.find_element_by_id ("password").send_keys("iamjust16")
     driver.find_element_by_tag_name("button").click()
 
-    driver.get("https://www.linkedin.com/in/maghil/edit/certification/new/")
+    #API to add new certificate
+    driver.get("https://www.linkedin.com/profile/add?startTask=CERTIFICATION_NAME&name=test1&organizationName=Coursera&issueYear=2018&issueMonth=2&expirationYear=2020&expirationMonth=5&certUrl=https%3A%2F%2Fdocs.microsoft.com%2Fen-us%2Flearn%2Fcertifications%2Fd365-functional-consultant-sales&certId=1234")
 
-    #wait until element is found
-    driver.implicitly_wait(200)
-
-    #certification name
-    driver.find_element_by_id("certification-name").send_keys("Coursera")
-
-    #certification issuer
-    el=driver.find_element_by_xpath("//div[@class='pe-artdeco-typeahead search-basic-typeahead search-vertical-typeahead relative pe-artdeco-typeahead__stack-index--3 ember-view']//input")
-    el.click()
-    el.send_keys("test1")
-    el.send_keys(Keys.ENTER)
-
+    time.sleep(5)
     #certification will not expire checkbox
     driver.find_element_by_xpath("//div[@class='mb3']//label").click()
 
-    #certification start date
-    driver.find_element_by_xpath("//select[@id='certification-start-month']/option[text()='December']").click()
-    driver.find_element_by_xpath("//select[@id='certification-start-year']/option[text()='2020']").click()
+    time.sleep(5)
 
-    #certification license number
-    driver.find_element_by_id("certification-license-number").send_keys("test2")
-
-    #certification url
-    driver.find_element_by_id("certification-url").send_keys("test3")
-
-    #certification save
+    #certification save button
     driver.find_element_by_class_name("pe-form-footer__action--submit artdeco-button form-submit-action t-14 t-white t-normal").click()
 
-    time.sleep(5)
+    
 
 except Exception as e:
     print(e)
