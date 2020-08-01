71a2e79885702eeb56c7c1eec93490f86f203135
Maghil <mmaghil2k@gmail.com>
2020-08-01 18:20:30 +0530

    still have to work on issue year and issue date

--- linkedin.py
+++ linkedin.py
@@ -6,36 +6,43 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 
-with open(r'linkedIn.yml') as file:
-    user = yaml.full_load(file)
+def deserializer():
+    with open(r'test.yml') as file:
+        user = yaml.full_load(file)
+    return(user)
 
-try:
-    driver = webdriver.Chrome(ChromeDriverManager().install())
-    
-    #login page
-    driver.get("https://www.linkedin.com/login?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin")
+def executor():
+    try:
+        user=deserializer()
+        driver = webdriver.Chrome(ChromeDriverManager().install())
+        
+        #login page
+        driver.get("https://www.linkedin.com/login?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin")
 
-    driver.find_element_by_id("username").send_keys(user["email"])
-    driver.find_element_by_id ("password").send_keys(user["pwd"])
-    driver.find_element_by_tag_name("button").click()
+        driver.find_element_by_id("username").send_keys(user["email"])
+        driver.find_element_by_id ("password").send_keys(user["pwd"])
+        driver.find_element_by_tag_name("button").click()
 
-    #API to add new certificate
-    driver.get("https://www.linkedin.com/profile/add?startTask=CERTIFICATION_NAME&name=test1&organizationName=Coursera&issueYear=2018&issueMonth=2&expirationYear=2020&expirationMonth=5&certUrl=https%3A%2F%2Fdocs.microsoft.com%2Fen-us%2Flearn%2Fcertifications%2Fd365-functional-consultant-sales&certId=1234")
+        #API to add new certificate
+        driver.get("https://www.linkedin.com/profile/add?startTask=CERTIFICATION_NAME&name="+user["name"]+"&organizationName="+user["org"]+"&issueYear=2018&issueMonth=2&expirationYear=2020&expirationMonth=5&certUrl="+user["lUrl"]+"&certId="+str(user["lNumber"]))
 
-    time.sleep(5)
-    #certification will not expire checkbox
-    driver.find_element_by_xpath("//div[@class='mb3']//label").click()
+        time.sleep(5)
+        #certification will not expire checkbox
+        driver.find_element_by_xpath("//div[@class='mb3']//label").click()
 
-    time.sleep(5)
+        time.sleep(5)
 
-    #certification save button
-    driver.find_element_by_class_name("pe-form-footer__action--submit artdeco-button form-submit-action t-14 t-white t-normal").click()
+        #certification save button
+        driver.find_element_by_class_name("pe-form-footer__action--submit artdeco-button form-submit-action t-14 t-white t-normal").click()
+        return(1)
 
-    
+    except Exception as e:
+        print(e)
+        return(0)
 
-except Exception as e:
-    print(e)
+    finally:
+        print("closing browser")
+        driver.close()
 
-finally:
-    print("closing browser")
-    driver.close()
+if __name__ == "__main__":
+    executor()
